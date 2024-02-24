6cf16783ca6020742db378fcbb4408ecf01f55a3
Lester Ong <lesterong776@gmail.com>
2024-02-24 23:53:00 +0800

    Adjust autocomplete field

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Autocomplete from "./components/Autocomplete.jsx";
 function App() {
   const defaultData = ['Apple', 'Orange', 'Grape', 'Pear', 'Blueberry', 'Pineapple']
   return (
-    <main className="grid grid-cols-2 max-w-3xl mx-auto gap-4 p-6 items-start justify-center h-dvh ">
+    <main className="grid sm:grid-cols-2 max-w-3xl mx-auto gap-4 p-6 items-start justify-center h-dvh">
       <Autocomplete dataSource={defaultData} label="Enabled"></Autocomplete>
       <Autocomplete disabled label="Disabled"></Autocomplete>
       <Autocomplete dataSource={defaultData} label="Enabled" description="This is an autocomplete field with a description!"></Autocomplete>
